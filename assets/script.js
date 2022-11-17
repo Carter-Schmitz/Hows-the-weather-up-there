f90282d55d11e4ea5875ec6e433099ac5034f6e1
Carter_Schmitz <carterschmitz95@gmail.com>
2022-11-16 18:55:01 -0600

    added then response to getweather function

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,36 @@
-    var cityEl = document.getElementById("enter-city");
-    var searchEl = document.getElementById("search-button");
-    var clearEl = document.getElementById("clear-history");
-    var nameEl = document.getElementById("city-name");
-    var currentPicEl = document.getElementById("current-pic");
-    var currentTempEl = document.getElementById("temperature");
-    var currentHumidityEl = document.getElementById("humidity");
-    var currentWindEl = document.getElementById("wind-speed");
-    var currentUVEl = document.getElementById("UV-index");
-    var historyEl = document.getElementById("history");
-    var fivedayEl = document.getElementById("fiveday-header");
-    var todayweatherEl = document.getElementById("today-weather");
-    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+var cityEl = document.getElementById("enter-city");
+var searchEl = document.getElementById("search-button");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindEl = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history");
+var fivedayEl = document.getElementById("fiveday-header");
+var todayweatherEl = document.getElementById("today-weather");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-    // Assigning a unique API to a variable
-    var APIKey = "026c2100da69b297d0506d8b4488fe4d";
+// Assigning a unique API to a variable
+var APIKey = "026c2100da69b297d0506d8b4488fe4d";
 
-    function getWeather(cityName) {
-        // Execute a current weather get request from open weather api
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-        axios.get(queryURL)
-            .then(function (response) {
+function getWeather(cityName) {
+    // Execute a current weather get request from open weather api
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    axios.get(queryURL)
+        .then(function (response) {
 
-                todayweatherEl.classList.remove("d-none");
+            todayweatherEl.classList.remove("d-none");
+            // Parse response to display current weather
+            var currentDate = new Date(response.data.dt * 1000);
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth() + 1;
+            var year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            var weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
